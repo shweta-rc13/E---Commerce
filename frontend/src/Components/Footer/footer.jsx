0e0f9e074f58f72465ec7949f50721b589645b79
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-03-04 23:30:36 +0530

    footer

--- frontend/src/Components/Footer/footer.jsx
+++ frontend/src/Components/Footer/footer.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const footer = () => {
   return (
     <div>
-      
+      <div className="footer">
+        <div className="container">
+            <div className="row">
+                
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
